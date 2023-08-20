d92841e816693e85af2b5d6a7cd6442bf75d2dda
Stephen Meriwether <stephen.meriwether@icloud.com>
2023-08-20 11:21:22 -0400

    Add "days away" counter to header
    Also account for the day of the wedding and after we're married.

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -13,4 +13,22 @@ document.addEventListener("DOMContentLoaded", function() {
   if (mobileMenuBtn) {
     mobileMenuBtn.addEventListener('click', menuClickHandler);
   }
+
+  setDaysAway();
 });
+
+function setDaysAway() {
+  const weddingDate = new Date(2024, 4, 25);
+  const today = new Date();
+  const daysAway = Math.round(Math.abs(weddingDate - today) / (24 * 60 * 60 * 1000));
+  if (daysAway === 0) {
+    const daysAwaySpan = document.querySelector('#wedding-date');
+    daysAwaySpan.innerHTML = "Today's the day!";
+  } else if (today > weddingDate) {
+    const daysAwaySpan = document.querySelector('#wedding-date');
+    daysAwaySpan.innerHTML = `We're married!`;
+  } else {
+    const daysAwaySpan = document.querySelector('#days-away');
+    daysAwaySpan.innerHTML = `${daysAway} days away`;
+  }
+}
